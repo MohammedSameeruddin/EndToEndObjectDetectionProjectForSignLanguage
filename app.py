d0e677e0ef45798d94b6ee8237769a5c4620718f
Sameeruddin.Mohammed <Sameeruddin.Mohammed@HMG.LOCAL>
2024-07-30 22:07:32 +0300

    data ingesion code has been addedd

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from signLanguage.logger import logging
 from signLanguage.exception import SignException
 import sys
+from signLanguage.pipeline.training_pipeline import TrainPipeline
 
-#logging.info("Welcome to python logging")
-
-try:
-
-    a= 7/6
-    print(a)
+obj = TrainPipeline()
+obj.run_pipeline()
 
 
-except Exception as e:
-    raise SignException(e, sys) from e
